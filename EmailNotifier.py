e25b6bb566eefb55c5957f3ac8fe4bb2bbe7f6c8
Mike Croft <backtotheoldhouse@gmail.com>
2014-06-16 19:55:21 +0100

    Completed emailer class

--- EmailNotifier.py
+++ EmailNotifier.py
@@ -3,23 +3,44 @@
 # From this SO answer http://stackoverflow.com/a/2020553/212224
 
 import smtplib
+import configparser
 
-SERVER = "localhost"
-FROM = ""
-TO = [""]
-SUBJECT = "Alert!"
-TEXT = "This message was sent with Python's smtplib."
+# config = configparser.ConfigParser()
+# config.read('emailconf.ini')
 
+class EmailNotifier:
 
-message = """\
-From: %s
-To: %s
-Subject: %s
+	config = configparser.ConfigParser()
+	config.read('emailconf.ini')
 
-%s
-""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+	def notify(self, subject, body):
 
-server = smtplib.SMTP(SERVER)
-server.set_debuglevel(3)
-server.sendmail(FROM, TO, message)
-server.quit()
+		SERVER = self.config['Sender']['SERVER']
+		USER = self.config['Sender']['USER']
+		PASS = self.config['Sender']['PASS']
+		FROM = self.config['Sender']['FROM']
+		TO = []
+		SUBJECT = subject
+		TEXT = body
+
+		for addr in self.config['Notify_List']:
+			TO.append(self.config['Notify_List'][addr])
+
+		server = smtplib.SMTP(SERVER, 587)
+		server.set_debuglevel(3)
+		server.login(USER, PASS)
+		server.sendmail(FROM, TO, self.compose(FROM, TO, subject, body))
+		server.quit()
+
+	def compose(self, FROM, TO, subject, body):
+
+		header  = 'From: %s\n' % FROM
+		header += 'To: %s\n' % ','.join(TO)
+		# header += 'Cc: %s\n' % ','.join(cc_addr_list)
+		header += 'Subject: %s\n\n' % subject
+		message = header + message
+
+		return message
+
+eNotify = EmailNotifier()
+eNotify.notify("Snapshot script failed!", "The script failed. You should investigate.")
